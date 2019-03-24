23d6cf00596d01db25cea320cb8c9e29062e49c6
Administrator <ysangy@gmail.com>
2019-03-24 12:18:33 -0400

    Iguanodon

--- src/DinoList.js
+++ src/DinoList.js
@@ -116,7 +116,7 @@ export default class DinoList extends Component {
                 </li>
                 <li className="timeline-item">
                     <div className="card">
-                        <img className="card-icon" src="https://png.pngtree.com/element_pic/16/10/31/8d5628a528a720539da8c606f6677b0d.jpg" alt="Vector cartoon green dinosaur  png clipart from pngtree.com" />
+                        <img className="card-icon" src="https://res.cloudinary.com/drrwovgv6/image/upload/v1553444284/iguanodon_ksypck.jpg" alt="Iguanodon" />
                         <h2 className="card-title">Iguanodon</h2>
                         <dl className="card-definition">
                             <dt>
