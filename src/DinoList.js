094b7be983ff15bc840df793d12a8acb12a6f837
Administrator <ysangy@gmail.com>
2019-03-24 12:15:58 -0400

    Herrerasaurus

--- src/DinoList.js
+++ src/DinoList.js
@@ -103,7 +103,7 @@ export default class DinoList extends Component {
                 </li>
                 <li className="timeline-item">
                     <div className="card">
-                        <img className="card-icon" src="https://png.pngtree.com/element_pic/16/10/31/8d5628a528a720539da8c606f6677b0d.jpg" alt="Vector cartoon green dinosaur  png clipart from pngtree.com" />
+                        <img className="card-icon" src="https://res.cloudinary.com/drrwovgv6/image/upload/v1553444125/herrerasaurus.jpq_z8z1bc.jpg" alt="Herrerasaurus" />
                         <h2 className="card-title">Herrerasaurus</h2>
                         <dl className="card-definition">
                             <dt>
